5079b612c75bba2677729cd90c47ecd7d350d546
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-18 22:08:49 -0500

    feat(shared): add route to notification details screen.

--- lib/shared/router/app_router.dart
+++ lib/shared/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:push_app/notifier/presentation/screens/details_screen.dart';
 import 'package:push_app/public/home/screens/home_screen.dart';
 
 final appRouter = GoRouter(
@@ -7,6 +8,12 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/push-details/:messageId',
+      builder: (context, state) => DetailsScreen(
+        pushMessageId: state.pathParameters['messageId'] ?? '',
+      ),
     ),
   ],
 );
